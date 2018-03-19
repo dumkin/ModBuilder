db1c8749ac85ecf962c411142504a5659ae1ff64
Danil Dumkin <danil@dumk.in>
2018-03-19 17:44:51 +0400

    1.0

---
+++ Form_Project.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ModBuilder
+{
+    public partial class Form_Project : Form
+    {
+        public Form_Project()
+        {
+            InitializeComponent();
+        }
+    }
+}
